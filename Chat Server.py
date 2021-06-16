2294715ee50b4f258ec94ada1e2d15df79370ddc
Bobby5x3 <80545254+KavinUmasankar@users.noreply.github.com>
2021-06-16 12:59:17 -0500

    Update Chat Server.py

--- Chat Server.py
+++ Chat Server.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 15 17:54:06 2021
-
-@author: kavin
-"""
 import socket
 from _thread import *
 
@@ -16,15 +10,10 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client1 = ""
 client2 = ""
 
+clientnames = {}
 clients = []
 
 def threaded_client(connection, client):
-    global client1
-    global client2
-    global sendTo0
-    global sendTo1
-    
-    
     
     connection.send(str.encode(str(client)))
     
@@ -34,16 +23,9 @@ def threaded_client(connection, client):
             data = data.decode("utf-8")
             
             if len(data) > 5 and data[0:4] == "name":
-                if client == 0:
-                    client1 = data[4:]
-                if client == 1:
-                    client2 = data[4:]
-                        
+                clientnames[client] = data[4:]
             else:
-                if client == 0:
-                    reply =  client1 + ": " + data + "\n"
-                if client == 1:
-                    reply =  client2 + ": " + data + "\n"
+                reply = clientnames[client] + ": " + data + "\n"
                 for user in clients:
                     user.sendall(str.encode(reply))
                 
